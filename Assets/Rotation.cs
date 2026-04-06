2105ce11d4f533d7ad9586f6766bec0b9ec63e95
tyllarBarr <itzcookiq@gmail.com>
2026-04-06 13:50:37 +0100

    done one side

--- Assets/Rotation.cs
+++ Assets/Rotation.cs
@@ -4,11 +4,13 @@ public class Rotation : MonoBehaviour
 {
     public float rotationSpeed = 100f;
 
+
+
     void Update()
     {
-        // if (Input.GetKey(KeyCode.Space))
-        // {
+         
+         
             transform.Rotate(0f, 0f, rotationSpeed * Time.deltaTime);
-        // }
+         
     }
 }
